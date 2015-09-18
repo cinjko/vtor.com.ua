f99e7cc531f78438730584c9753c4e2cae8550bd
artem <cinjko21@gmail.com>
2015-09-19 00:01:17 +0300

    Ьigration is made for purchases table.

--- console/migrations/m150918_205216_create_purchases_table.php
+++ console/migrations/m150918_205216_create_purchases_table.php
@@ -7,14 +7,21 @@ class m150918_205216_create_purchases_table extends Migration
 {
     public function up()
     {
-
+        $this->createTable('tbl_purcheses',[
+           'id' => Schema::TYPE_PK,
+            'title_ua'       => Schema::TYPE_STRING.' NOT NULL',
+            'title_ru'       => Schema::TYPE_STRING.' NOT NULL',
+            'title_en'       => Schema::TYPE_STRING.' NOT NULL',
+            'description_ua' => Schema::TYPE_TEXT,
+            'description_ru' => Schema::TYPE_TEXT,
+            'description_en' => Schema::TYPE_TEXT,
+            'image'          => Schema::TYPE_STRING.' NOT NULL'
+        ]);
     }
 
     public function down()
     {
-        echo "m150918_205216_create_purchases_table cannot be reverted.\n";
-
-        return false;
+        $this->dropTable('tbl_purcheses');
     }
 
     /*
